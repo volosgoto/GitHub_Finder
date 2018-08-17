1399d4f5e0a55229ae9a8935be51de86b03057d7
volosgoto <volosgoto@yahoo.com>
2018-08-17 12:28:02 +0300

    add search listenet for username

--- app.js
+++ app.js
@@ -2,3 +2,16 @@
  * GitHub Finder
  * @version 1.0
  */
+
+ // Search input
+ let searchUser = document.getElementById('searchUser');
+ 
+ // Search User
+ searchUser.addEventListener('keyup', (e)=>{
+    // Get input text
+    let userText = e.target.value;
+    if (userText !== '') {
+        console.log(userText);
+        
+    }
+ });
