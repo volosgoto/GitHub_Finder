b1ba9be0a16a1370c240a7963f1e29ac035886b8
volosgoto <volosgoto@yahoo.com>
2018-08-20 08:48:10 +0300

    add user profile and repos

--- app.js
+++ app.js
@@ -5,31 +5,33 @@
 
 // Init
 let github = new Github();
+let ui = new Ui();
 
  // Search input
- let searchUser = document.getElementById('searchUser');
+let searchUser = document.getElementById('searchUser');
  
- // Search User
- searchUser.addEventListener('keyup', (e)=>{
+// Search User
+searchUser.addEventListener('keyup', (e)=>{
     // Get input text
     let userText = e.target.value;
     if (userText !== '') {
         // console.log(userText);
         // Make http call
-        github.getUser(userText)
+        github.getUser(userText) // returns Promise
             .then(data => {
-                
                 // console.log(data);
-                               
                 if (data.profile.messge == 'Not Found'){
                     // Show alert
+                    ui.showAlert('User not found', 'alert alert-danger');
                 } else {
                     //Show pofile
+                    ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
                 }
-
             }
         );
     } else {
         // Clear profile
+        ui.clearProfile();
     }
- });
+});
